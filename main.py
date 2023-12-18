457d67e181512879b06893132710b1659940edbd
Matheus <mtpaivamrhenrique@gmail.com>
2023-12-18 19:03:32 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.");
+print("What's name of the city you grew up in?")
+city = input();
+print("What's your pet1s name?");
+pet = input();
+
+print("Your band name could be " + city + " " + pet);
